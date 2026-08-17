494408b6b1329daba60d59ed83db4778f3777517
faizaladla09-tech <faizaladla09@gmail.com>
2026-08-17 18:17:51 +0700

    Jnm

--- app/developer/page.tsx
+++ app/developer/page.tsx
@@ -33,7 +33,7 @@ export default function DeveloperPage() {
             <div className="absolute inset-0 bg-[#224032] rounded-[60%_40%_30%_70%_/_50%_60%_40%_50%] animate-[blob_8s_ease-in-out_infinite_reverse] scale-105" />
             <div className="relative w-full h-full rounded-[50%_50%_40%_60%_/_60%_40%_50%_50%] overflow-hidden border-2 border-[#4ADE80]/20 z-10">
               <Image 
-                src="https://d.uguu.se/dMfkZGpi.jpeg" 
+                src="https://uploader.renn.my.id/renn/cdn_923573.jpg" 
                 alt="FAIZAL GRUB" 
                 fill 
                 sizes="192px"
